c47146421aa92dbc370d55eb6d5670e3cc88ef5b
Informatica 10 <informatica10@live.local>
2021-08-30 16:56:01 -0300

    Inserção programas menu

---
+++ src/main/java/br/com/live/sql/tables/release00004/006_insercao_programas_menu.sql
@@ -0,0 +1,5 @@
+INSERT INTO orion_002 (id, descricao, modulo, path)
+VALUES (8,'Cadastro de Programas','BI','/bi-programas');
+
+INSERT INTO orion_002 (id, descricao, modulo, path)
+VALUES (9,'Cadastro de Usuários','BI','/bi-usuarios');
