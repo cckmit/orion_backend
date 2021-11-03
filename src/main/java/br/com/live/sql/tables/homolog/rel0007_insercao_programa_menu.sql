271851dbf622527ad6d87cc1169e558b16ba8d63
Rodrigo <rodrigo.ti@liveoficial.com.br>
2021-11-03 11:14:32 -0300

    .

--- src/main/java/br/com/live/sql/tables/homolog/rel0007_insercao_programa_menu.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO orion_002 (id, descricao, modulo, path)
-VALUES (14,'Calendário de Produção','Confecção','/calendario-producao');
-
-INSERT INTO orion_002 (id, descricao, modulo, path)
-VALUES (15,'Estações','Comercial','/estacoes');
-
-INSERT INTO orion_002 (id, descricao, modulo, path)
-VALUES (16,'Agrupador de Coleção','Comercial','/agrupador');
