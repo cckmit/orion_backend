4e839fd8d0c645805c4b38dce3df791f917a8746
Joao <joao.ti@liveoficial.com.br>
2022-09-13 09:04:01 -0300

    Ajustes SQL

--- src/main/java/br/com/live/sql/tables/homolog/rel0027_nova_tabela_restricoes.sql
+++ src/main/java/br/com/live/sql/tables/homolog/rel0027_nova_tabela_restricoes.sql
@@ -9,6 +9,8 @@ CREATE TABLE orion_cfc_271 (
 			 id number(9),
 			 codigo_rolo number(9),
              restricao number(9),
-             constraint orion_cfc_271_pk PRIMARY KEY(id));
+             constraint orion_cfc_271_pk PRIMARY KEY(id),
+             constraint fk_orion_cfc_271_orion_cfc_270 foreign key (restricao) references orion_cfc_270 (id)
+             );
 
 comment on table orion_cfc_271 is 'Tabela do cadastro de Restrições por Rolo';
