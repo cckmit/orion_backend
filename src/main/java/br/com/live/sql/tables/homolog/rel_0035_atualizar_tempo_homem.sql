986b4542ea058ed67d68f9fb4c4f17fdb983f3d5
Marcos Regis <romano.marcosregis@gmail.com>
2023-01-17 16:28:34 -0300

    Alterado

--- src/main/java/br/com/live/sql/tables/homolog/rel_0035_atualizar_tempo_homem.sql
+++ src/main/java/br/com/live/sql/tables/homolog/rel_0035_atualizar_tempo_homem.sql
@@ -1,1 +1,7 @@
-UPDATE mqop_040 a SET a.tempo_homem = a.tempo_homem + ((1.86 * a.tempo_homem) / 100) WHERE a.codigo_operacao BETWEEN 20000 AND 30000
+-- EXECUTAR ESSE SCRIPT PRIMEIRO
+UPDATE mqop_040 a SET a.tempo_homem = a.tempo_homem + ((1.86 * a.tempo_homem) / 100) 
+       WHERE a.codigo_operacao BETWEEN 20000 AND 30000;
+COMMIT;
+-- EXECUTAR EM SEGUIDA
+UPDATE mqop_050 v SET v.minutos_homem = (SELECT b.tempo_homem FROM mqop_040 b WHERE b.codigo_operacao = v.codigo_operacao) WHERE v.codigo_operacao BETWEEN 20000 AND 30000;
+COMMIT;
