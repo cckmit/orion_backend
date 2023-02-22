2402a77891f3c543f52af07ab140794bd5d46ba7
Marcos Regis <romano.marcosregis@gmail.com>
2023-02-22 15:36:08 -0300

    ajustes

--- src/main/java/br/com/live/sql/tables/homolog/inclusao_programas.sql
+++ src/main/java/br/com/live/sql/tables/homolog/inclusao_programas.sql
@@ -238,3 +238,8 @@ values (78, 'Pré Cadastro Indicadores', 'Administrativo', '/pre-cadastro-indica
 insert into orion_002 (ID, DESCRICAO, MODULO, PATH)
 values (79, 'Painel de Sequenciamento Decorações', 'Confecção', '/painel-seq-decoracoes');
 
+insert into orion_002 (ID, DESCRICAO, MODULO, PATH)
+values (80, 'Encolhimento CAD', 'Confecção', '/encolhimento-cad');
+   
+insert into orion_002 (ID, DESCRICAO, MODULO, PATH)
+values (81, 'Cópia de Parâmetros para Ficha de Custos', 'Custos', '/copia-param-custos');
