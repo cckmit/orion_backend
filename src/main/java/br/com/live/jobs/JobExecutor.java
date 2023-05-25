dd7269e69d94903c77a8a5c7d0fd1b151c4b4152
Henrique Rueckert <henrique.rueckert@live.local>
2023-05-25 12:04:09 -0300

    .

--- src/main/java/br/com/live/jobs/JobExecutor.java
+++ src/main/java/br/com/live/jobs/JobExecutor.java
@@ -1,11 +1,21 @@
 package br.com.live.jobs;
 
+import br.com.live.service.TituloPagamentoService;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableScheduling
 public class JobExecutor {
 
+    private final static int SEGUNDO = 1000;
+    private final static int MINUTO = 60000;
+    private final static int HORA = 3600000;
+    private final static int DIA = HORA * 24;
 
+    @Scheduled(fixedRate = MINUTO)
+    public void gerarTitulosNFSePrefeitura(){
+        //tituloPagamentoService.gerarTitulosNFSePrefeituraJob();
+    }
 }
