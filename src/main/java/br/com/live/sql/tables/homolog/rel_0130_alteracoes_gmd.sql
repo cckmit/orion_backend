9e93198ea3263f654e47bb25765c54976ca9e2a4
Rodrigo Furlaneto <rodrigo.ti@liveoficial.com.br>
2023-08-17 11:48:24 -0300

    sql para o projeto gmd e nova procedure para congelamento de pedidos

--- src/main/java/br/com/live/sql/tables/homolog/rel_0130_alteracoes_gmd.sql
+++ src/main/java/br/com/live/sql/tables/homolog/rel_0130_alteracoes_gmd.sql
@@ -3,11 +3,20 @@ SQL 1) ORION -------------------------------------------------------------------
 -- atualizar mqop_160
 delete from mqop_045;
 delete from mqop_160;
-insert into mqop_160 (select m.codigo_operacao codigo, 'MICROMOVIMENTO GENERICO' descricao, 0 utiliza_maquina, m.tempo_homem tempo, 1 frequencia, 1 quantidade from mqop_040 m);
+insert into mqop_160 (select m.codigo_operacao codigo, 'MICROMOVIMENTO GENERICO' descricao, 0 utiliza_maquina, m.tempo_homem tempo, 1 frequencia, 1 quantidade from mqop_040 m where m.categoria_pagamento <> 'P');
 
 -- atualizar mqop_045
-insert into mqop_045 (select m.codigo_operacao, m.codigo_operacao codigo_movimento, 1 sequencia, 1 quantidade, 1 frequencia, m.tempo_homem tempo_total  from mqop_040 m);
+insert into mqop_045 (select m.codigo_operacao, m.codigo_operacao codigo_movimento, 1 sequencia, 1 quantidade, 1 frequencia, m.tempo_homem tempo_total  from mqop_040 m where m.categoria_pagamento <> 'P');
 
+
+delete from mqop_045
+where mqop_045.codigo_operacao in (select m.codigo_operacao from mqop_040 m
+                                    where m.categoria_pagamento = 'P')
+;
+delete from mqop_160
+where mqop_160.codigo in (select m.codigo_operacao from mqop_040 m
+                           where m.categoria_pagamento = 'P')
+;
 
 SQL 2) SYSTEXTIL ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -18,14 +27,24 @@ CREATE OR REPLACE TRIGGER "LIVE_TR_MQOP_040_1"
 /* RODRIGO F. - 37988 (GMD). QUANDO CADASTRAR UMA NOVA OPERAÇÃO, DEVE INSERIR AUTOMATICAMENTE UM MICROMOVIMENTO GENERICO. */
 
 BEGIN
-   BEGIN
-      INSERT INTO mqop_160 (codigo, descricao, utiliza_maquina, tempo, frequencia, quantidade)
-                    VALUES (:new.codigo_operacao, 'MICROMOVIMENTO GENERICO', 0, 0, 1, 1);
-   END;
-   BEGIN
-      INSERT INTO mqop_045 (codigo_operacao, codigo_movimento, sequencia, quantidade, frequencia, tempo_total)
-                    VALUES (:new.codigo_operacao, :new.codigo_operacao, 1, 1, 1, 0);
-   END;
+
+   IF (:NEW.categoria_pagamento is null) OR (:NEW.categoria_pagamento <> 'P')
+   THEN
+
+      BEGIN
+         INSERT INTO mqop_160 (codigo, descricao, utiliza_maquina, tempo, frequencia, quantidade)
+                       VALUES (:new.codigo_operacao, 'MICROMOVIMENTO GENERICO', 0, 0, 1, 1);
+         EXCEPTION
+            WHEN OTHERS THEN NULL;
+      END;
+      BEGIN              
+         INSERT INTO mqop_045 (codigo_operacao, codigo_movimento, sequencia, quantidade, frequencia, tempo_total)
+                       VALUES (:new.codigo_operacao, :new.codigo_operacao, 1, 1, 1, 0);
+         EXCEPTION
+            WHEN OTHERS THEN NULL;                       
+      END;
+   
+   END IF;
 END LIVE_TR_MQOP_040_1;
 
 SQL 3) ORION --------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- src/main/java/br/com/live/sql/tables/producao/rel0121_nova_procedure.sql
+++ src/main/java/br/com/live/sql/tables/producao/rel0121_nova_procedure.sql
@@ -1,10 +1,17 @@
 CREATE OR REPLACE PROCEDURE "ORION_PR_CONGELA_PEDIDOS"
 IS
    CURSOR cur_pedidos IS
-      SELECT a.pedido_venda, a.data_entr_venda, a.situacao_venda
+      SELECT a.pedido_venda,       a.data_entr_venda, a.situacao_venda,   a.cod_cancelamento,
+             a.sit_aloc_pedi,      a.sit_coletor,     a.situacao_coleta,  a.status_expedicao, 
+             a.status_homologacao, a.status_pedido,   a.status_comercial, a.sit_conferencia
         FROM pedi_100 a
-       WHERE a.cod_cancelamento = 0
-         AND a.data_entr_venda = TRUNC(SYSDATE-1);
+       WHERE a.data_entr_venda = TRUNC(SYSDATE-1)
+         AND a.qtde_total_pedi > 0;
+
+   CURSOR cur_motiv_bloqueio (p_pedido number) IS
+      SELECT a.pedido_venda, a.seq_situacao, a.codigo_situacao, a.flag_liberacao, a.data_situacao, a.data_liberacao
+        FROM pedi_135 a
+       WHERE a.pedido_venda = p_pedido;
 
 -- Rodrigo F.
 -- Congela os pedidos com embarque do dia anterior, para conferência e análise em relatórios e BI´s.
@@ -15,11 +22,27 @@ BEGIN
    LOOP
 
       BEGIN
-         INSERT INTO orion_com_310 (PEDIDO_VENDA,             DATA_ENTR_VENDA,             SITUACAO_VENDA)
-                            VALUES (reg_pedidos.pedido_venda, reg_pedidos.data_entr_venda, reg_pedidos.situacao_venda);
+         INSERT INTO orion_com_310 (PEDIDO_VENDA,                   DATA_ENTR_VENDA,             SITUACAO_VENDA,              COD_CANCELAMENTO,
+                                    SIT_ALOC_PEDI,                  SIT_COLETOR,                 SITUACAO_COLETA,             STATUS_EXPEDICAO, 
+                                    STATUS_HOMOLOGACAO,             STATUS_PEDIDO,               STATUS_COMERCIAL,            SIT_CONFERENCIA)
+                            VALUES (reg_pedidos.pedido_venda,       reg_pedidos.data_entr_venda, reg_pedidos.situacao_venda,  reg_pedidos.cod_cancelamento,                            
+	                                  reg_pedidos.sit_aloc_pedi,      reg_pedidos.sit_coletor,     reg_pedidos.situacao_coleta, reg_pedidos.status_expedicao, 
+                                    reg_pedidos.status_homologacao, reg_pedidos.status_pedido,   reg_pedidos.status_comercial,reg_pedidos.sit_conferencia);
          EXCEPTION
             WHEN OTHERS THEN NULL;
-      END;
+      END;            
+            
+      FOR reg_motiv_bloqueio IN cur_motiv_bloqueio(reg_pedidos.pedido_venda)
+      LOOP         
+
+         BEGIN
+            INSERT INTO orion_com_311 (PEDIDO_VENDA, SEQ_SITUACAO, CODIGO_SITUACAO, FLAG_LIBERACAO, DATA_SITUACAO, DATA_LIBERACAO) 
+                               VALUES (REG_MOTIV_BLOQUEIO.PEDIDO_VENDA, REG_MOTIV_BLOQUEIO.SEQ_SITUACAO, REG_MOTIV_BLOQUEIO.CODIGO_SITUACAO, REG_MOTIV_BLOQUEIO.FLAG_LIBERACAO, REG_MOTIV_BLOQUEIO.DATA_SITUACAO, REG_MOTIV_BLOQUEIO.DATA_LIBERACAO);
+	          EXCEPTION
+               WHEN OTHERS THEN NULL;                  
+         END;
+         
+      END LOOP;
 
       COMMIT;
    END LOOP;
